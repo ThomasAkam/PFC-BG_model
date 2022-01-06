cd1914a899bedcffd3bbc8070ba3cd6862d70f73
takam <thomas.akam@psy.ox.ac.uk>
2022-01-06 16:19:30 +0000

    - Version of PFC RL model where PFC takes state, reward and action as intputs and predits state and reward.  Not working as catagorical_cross_entropy loss is not suitable for this setup.

--- PFC_RL_model.py
+++ PFC_RL_model.py
@@ -1,6 +1,7 @@
 #%% Imports
 
 import numpy as np
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import pylab as plt
@@ -11,57 +12,59 @@ import Two_step_task as ts
 #%% Parameters.
 
 # Simulation parameters.
-n_steps = 10000
+n_episodes = 1000
+n_epochs = 3
+episode_len = 100  # Episode length in trials.
+gamma = 0.9        # Discount rate
+max_step_per_episode = 600
+entropy_loss_weight = 0.01
 
 #Task params.
 good_prob = 0.99
 block_len = [20,21]
 
 # PFC model parameters.
-n_back = 30
-n_lstm = 16
-pfc_learning_rate = 0.05
+n_back = 4
+n_pfc = 12
+pfc_learning_rate = 0.01
 
 # Striatum model parameters.
+n_str = 12
 str_learning_rate = 0.05
-ave_reward_learning_rate = 0.01
 
 #%% Instantiate task.
 
-task = ts.Two_step(good_prob=good_prob, block_len=block_len)
+task = ts.Two_step(good_prob=good_prob, block_len=block_len, punish_invalid=False)
 
 #%% PFC model.
 
-pfc_inputs = layers.Input(shape=(n_back, task.n_states+task.n_actions)) # Inputs are 1 hot encoding of alternating states and actions.
-lstm = layers.LSTM(n_lstm, unroll=True, name='lstm', return_state=True)(pfc_inputs) # Recurrent layer.
-state_pred = layers.Dense(task.n_states, activation='softmax', name='state_pred')(lstm[0]) # Output layer predicts next state
+pfc_inputs = layers.Input(shape=(n_back, task.n_states+task.n_actions+1)) # Inputs are 1 hot encoding of alternating states and actions.
+lstm = layers.LSTM(n_pfc, unroll=True, name='lstm', return_state=True)(pfc_inputs) # Recurrent layer.
+state_pred = layers.Dense(task.n_states+1, activation='softmax', name='state_pred')(lstm[0]) # Output layer predicts next state
 PFC_model = keras.Model(inputs=pfc_inputs, outputs=state_pred)
-optimizer = keras.optimizers.Adam(learning_rate=pfc_learning_rate)
-PFC_model.compile(loss="categorical_crossentropy", optimizer=optimizer)
+pfc_optimizer = keras.optimizers.Adam(learning_rate=pfc_learning_rate)
+PFC_model.compile(loss="categorical_crossentropy", optimizer=pfc_optimizer)
 # Model variant used to get state of LSTM layer.
 PFC_state_view = keras.Model(inputs=PFC_model.input,
                              outputs=PFC_model.get_layer('lstm').output[2])
 
-pfc_input_buffer = np.zeros([n_back, task.n_states+task.n_actions])
+pfc_input_buffer = np.zeros([n_back, task.n_states+task.n_actions+1])
 
-def update_pfc_input_buffer(s=None, a=None):
-    '''Given an integer state or action update the pfc_input_buffer array.'''
+def update_pfc_inputs(s,r,a):
+    '''Update the inputs to the PFC network given the state,reward and action.'''
     global pfc_input_buffer
     pfc_input_buffer = np.roll(pfc_input_buffer,-1, axis=0)
     pfc_input_buffer[-1,:] = 0
-    if s is not None: # One-hot encoding of state s.
-        pfc_input_buffer[-1,s] = 1
-    else: # One hot encoding of action a.
-        pfc_input_buffer[-1,a+task.n_states] = 1
+    pfc_input_buffer[-1,s] = 1               # One hot encoding of old state.
+    pfc_input_buffer[-1,a+task.n_states] = 1 # One hot encoding of action.
+    pfc_input_buffer[-1,-1] = r
 
-def get_state_features_orig(s):
-    '''Get the new PFC features for states s and return feature vector S_op
-    combining observable and PFC features.'''
-    update_pfc_input_buffer(s=s)
-    S_op = np.zeros(task.n_states+n_lstm) # State feature vector combining observable and PFC features.
-    S_op[s] = 1 # Observable task state feature.
-    S_op[task.n_states:] = PFC_state_view(pfc_input_buffer[np.newaxis,:,:]).numpy() # PFC features
-    return S_op
+def get_state_features(s):
+    '''Get the state feature vectors combining observable and PFC features.'''
+    Sf = np.zeros(task.n_states+n_pfc) # State feature vector combining observable and PFC features.
+    Sf[s] = 1 # Observable task state feature.
+    # Sf[task.n_states:] = PFC_state_view(pfc_input_buffer[np.newaxis,:,:]) # PFC features
+    return Sf
 
 def pretrain_model(n_steps=40000, fig_no=1):
     # Generate data.
@@ -96,77 +99,106 @@ def pretrain_model(n_steps=40000, fig_no=1):
     plt.plot(ch_state_pc1 , label='lstm state PC1')
     return pca
 
-pca = pretrain_model()
-    
-def get_state_features(s):
-    '''Get the new PFC features for states s and return feature vector S_op
-    combining observable and PFC features.'''
-    update_pfc_input_buffer(s=s)
-    S_op = np.zeros(task.n_states+3) # State feature vector combining observable and PFC features.
-    S_op[s] = 1 # Observable task state feature.
-    pc1 = pca.transform(PFC_state_view(pfc_input_buffer[np.newaxis,:,:]).numpy())
-    if s == ts.choice:
-        S_op[task.n_states] = pc1
-    elif s == ts.sec_step_A:
-        S_op[task.n_states + 1] = pc1
-    elif s == ts.sec_step_B:
-        S_op[task.n_states + 2] = pc1
-    return S_op
 
-#%% Striatum model
+#%% Basal ganglia model
 
-#w_c = np.zeros(task.n_states+n_lstm)                   # Critic weights.
-#w_a = np.zeros([task.n_actions, task.n_states+n_lstm]) # Actor weights.
+str_inputs = layers.Input(shape=(task.n_states+n_pfc,))
+common = layers.Dense(n_str, activation="relu")(str_inputs)
+actor = layers.Dense(task.n_actions, activation="softmax")(common)
+critic = layers.Dense(1)(common)
 
-w_c = np.zeros(task.n_states+3)                   # Critic weights.
-w_a = np.zeros([task.n_actions, task.n_states+3]) # Actor weights.
-
-r_ave = 0 # Average reward.
-r_per_step = 0
-
-def choice_probs(S,w_a):
-    '''Given state feature vector S and actor weights w_a return choice probabilities.'''
-    return np.exp(w_a @ S)/np.sum(np.exp(w_a @ S))
-
-def log_chosen_prob_grad(a,S,w_a):
-    '''Given integer action a, state feature vector S and actor weights w_a,
-    return the gradient of log(choice_probs(S,w_a)[a]) with respect to w_a.'''
-    grad = -choice_probs(S,w_a)[:,np.newaxis]*S
-    grad[a,:] += S
-    return grad
+Str_model = keras.Model(inputs=str_inputs, outputs=[actor, critic])
+str_optimizer = keras.optimizers.Adam(learning_rate=str_learning_rate)
 
 #%% Environment loop.
 
-s = task.reset() # Get initial state as integer.
-S_op = get_state_features(s)
+sse_loss = keras.losses.MeanSquaredError(reduction=tf.keras.losses.Reduction.SUM)
 
-for i in range(n_steps):
+def store_history(s, Sf, r, a):
+    'Store state, reward and subseqent action for current timestep.'
+    global states, state_f, rewards, actions
+    states.append(s)
+    state_f.append(Sf)
+    rewards.append(r)
+    actions.append(a)
+    pfc_input.append(pfc_input_buffer)
+
+s = task.reset() # Get initial state as integer.
+r = 0
+Sf = get_state_features(s)
+
+for e in range(n_episodes):
     
-    # Choose action.
-    a = np.random.choice(task.n_actions, p=choice_probs(S_op, w_a))
+    step_n = 0
+    start_trial = task.trial_n
     
-    # Get next state and reward.
-    s, r, block_info = task.step(a)
+    # Episode history variables
+    states  = [] # int
+    state_f = [] # State feature vectors.
+    rewards = []
+    actions = [] # int
+    pfc_input = []
     
-    # Train PFC to predict next state.
-    update_pfc_input_buffer(a=a)
-    if False: # i > 100:
-        PFC_model.train_on_batch(x=pfc_input_buffer[np.newaxis,:,:],
-                             y=keras.utils.to_categorical(s, task.n_states)[np.newaxis,:])
+    while True:
+        step_n += 1
+        
+        choice_probs, V = Str_model(tf.convert_to_tensor(Sf[np.newaxis,:]))
+        
+        # Choose action.
+        a = np.random.choice(task.n_actions, p=np.squeeze(choice_probs))
+        
+        # Store history.
+        store_history(s, Sf, r, a)
+        
+        # Update the PFC networks inputs.
+        update_pfc_inputs(s,r,a)
+        
+        # Get next state and reward.
+        s, r = task.step(a)
+        
+        # Get new state vector conbining observable and PFC features.
+        Sf = get_state_features(s)
+        
+        episode_trials = task.trial_n - start_trial
+        if episode_trials == episode_len or step_n >= max_step_per_episode and s == 0:
+            break # End of episode.        
+
+    # Update Striatum weights.
     
-    # Get new state features.
-    S_new = get_state_features(s)
+    returns = np.zeros([len(rewards),1], dtype='float32')
+    returns[-1] = r
+    for i in range(1, len(returns)):
+        returns[-i-1] = rewards[-i] + gamma*returns[-i]
+         
+    for i in range(n_epochs):
+        with tf.GradientTape() as tape:
+            choice_probs, values = Str_model(tf.convert_to_tensor(state_f))
+            advantages = returns - values
+            # Critic loss.
+            critic_loss = sse_loss(values, returns)
+            # Actor loss
+            actor_losses = []
+            for i,a in enumerate(actions):
+                log_chosen_prob = tf.math.log(choice_probs[i,a])
+                entropy = -tf.reduce_sum(choice_probs[i,:]*tf.math.log(choice_probs[i,:]))
+                actor_losses.append(-log_chosen_prob*advantages[i]-entropy*entropy_loss_weight)
+            actor_loss = tf.reduce_sum(actor_losses)
+            # Apply combined loss.    
+            grads = tape.gradient(critic_loss+actor_loss, Str_model.trainable_variables)
+            str_optimizer.apply_gradients(zip(grads, Str_model.trainable_variables))
+            
+        # Update PFC weights
+        y = np.hstack([keras.utils.to_categorical(states, task.n_states), 
+                       np.array(rewards, 'float32')[:,np.newaxis]])
+        tl = PFC_model.train_on_batch(np.array(pfc_input),y)
     
-    # Update striatum model.
-    d = r - r_ave + w_c @ S_new - w_c @ S_op     # Reward prediction error.
-    r_ave = r_ave + ave_reward_learning_rate * d # Update average reward.
-    w_c = w_c + str_learning_rate * d * S_op     # Update critic weights.
-    w_a = w_a + str_learning_rate * d * log_chosen_prob_grad(a, S_op, w_a) # Update actor weights
+    print(f'Episode: {e} Steps: {step_n} Trials: {episode_trials} '
+          f' Rew. per tr.: {np.sum(rewards)/episode_trials :.2f} PFC tr. loss: {tl :.3f}')
     
-    S_op = S_new.copy()
     
-    r_per_step = r_per_step + ave_reward_learning_rate*(r-r_per_step)
     
-    if i % 100 == 0:
-        print(f'Step: {i} Reward per step: {r_per_step :.3f} r_ave:{r_ave :.3f}')
+    
+    
+    
+    
     
